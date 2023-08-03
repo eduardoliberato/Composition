fb9cfb29819afc4e79c3eb43eb8a0343111a1e35
Eduardo Liberato <64651176+eduardoliberato@users.noreply.github.com>
2023-08-03 19:34:56 -0300

    Add files via upload

---
+++ entities_composition/Department.java
@@ -0,0 +1,26 @@
+package entities_composition;
+
+public class Department {
+
+    private String name;
+
+    public Department(){
+
+    }
+
+    public Department(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String setName(String name){
+        return this.name = name;
+    }
+
+
+    
+}
+
---
+++ enum_composition/WorkerLevel.java
@@ -0,0 +1,9 @@
+package enum_composition;
+
+public enum WorkerLevel {
+
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+    
+}
